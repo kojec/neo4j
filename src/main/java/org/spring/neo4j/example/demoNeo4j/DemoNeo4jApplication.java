d9c4f99a2803a13582eb905f86035f621b1b26cc
Dzierzanowski <tomasz.dzierzanowski@hpe.com>
2017-07-28 16:46:16 +0200

    start

---
+++ src/main/java/org/spring/neo4j/example/demoNeo4j/DemoNeo4jApplication.java
@@ -0,0 +1,12 @@
+package org.spring.neo4j.example.demoNeo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNeo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoNeo4jApplication.class, args);
+	}
+}
